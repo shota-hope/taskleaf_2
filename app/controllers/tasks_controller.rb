b7d591dc2e9f83e35c5f71324318a04dbe5f6e56
ohno <ddeeppaa.gogo@gmail.com>
2021-06-26 18:02:09 +0900

    Fnish ch5-8

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = current_user.tasks.order(created_at: :desc)
   end
 
   def show
-    @task = current_user.tasks.find(params[:id])
   end
 
   def new
@@ -22,19 +23,16 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = current_user.tasks.find(params[:id])
   end
 
   def update
-    @task = current_user.tasks.find(params[:id])
     @task.update!(task_params)
     redirect_to tasks_url, notice: "タスク「#{@task.name}」を更新しました"
   end
 
   def destroy
-    task = Task.find(params[:id])
-    task.destroy
-    redirect_to tasks_url, notice: "タスク「#{task.name}」を削除しました"
+    @task.destroy
+    redirect_to tasks_url, notice: "タスク「#{@task.name}」を削除しました"
   end
 
   private
@@ -43,4 +41,7 @@ class TasksController < ApplicationController
     params.require(:task).permit(:name, :description)
   end
 
+  def set_task
+    @task = current_user.tasks.find(params[:id])
+  end
 end
