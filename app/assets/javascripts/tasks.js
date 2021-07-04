5376bf8d8f119d27eac62e7fe8e45606233a7f75
ohno <ddeeppaa.gogo@gmail.com>
2021-07-04 23:57:54 +0900

    finish ch8

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -9,14 +9,3 @@ document.addEventListener('turbolinks:load', function() {
     });
   });
 });
-
-document.addEventListener('turbolinks:load', function() {
-  document.querySelectorAll('.delete').forEach(function(a) {
-    a.addEventListener('ajax:success', function () {
-      var td = a.parentNode;
-      var tr = td.parentNode;
-      tr.style.display ='none';
-
-    });
-  });
-});
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -38,7 +38,6 @@ class TasksController < ApplicationController
 
   def destroy
     @task.destroy
-    head :no_content
   end
 
   def confirm_new
